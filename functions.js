44dd5e5d754e5a78a6b14cf0baf64d0ba558dfc9
Vincent <153116267+Vincent9956@users.noreply.github.com>
2024-03-07 09:51:29 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,21 @@
+//declaring
+function greetings (){
+    console.log("Greetings");
+}
+//calling the function 
+greetings();
+
+//passing values and constants
+function area(number = 12){
+    console.log(number + 2);
+}
+area();
+
+// No order
+
+Order();
+
+function Order(){
+    console.log("there is no order");
+} 
+//=> You do not have to declare before you call the function 
